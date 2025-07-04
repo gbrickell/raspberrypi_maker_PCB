716578117dc9f0888da047ee82f211140a8364d5
Geoff Brickell <geoff@brickell.org.uk>
2025-07-04 09:11:11 +0100

    add touch keypad test files

---
+++ Raspberry_Pi_SBC_code/sensors/TTP229_touch_keypad/TTP229_test01.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# TTP229_test01.py shows how the TTP229 touch keypad is used 
+#  uses two of the spare GPIO poins onthe PCB
+# Author : Enmore Green Limited
+# Date   : 210511
+# command to run this script:  python3 ./RPi_maker_kit5/sensors/TTP229_touch_keypad/TTP229_test01.py
+#  command above to be updated for the user's path to the code
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+SCLPin=18
+SDOPin=15
+
+HALF_BIT_TIME=.001
+CHARACTER_DELAY=5*HALF_BIT_TIME
+
+NUM_BITS=16
+
+GPIO.setup(SCLPin,GPIO.OUT)
+GPIO.setup(SDOPin,GPIO.IN)
+
+GPIO.output(SCLPin,GPIO.HIGH)
+time.sleep(HALF_BIT_TIME)
+oldKey=18
+
+try:
+    while True:
+        button=1
+        time.sleep(CHARACTER_DELAY)
+
+
+        while button < 17:
+            print_button=button
+            if (print_button==17):
+                print_button=1
+
+            GPIO.output(SCLPin,GPIO.LOW)
+            time.sleep(HALF_BIT_TIME)
+            keyval=GPIO.input(SDOPin)
+            if not keyval and not pressed:
+                pressed=True
+                if(oldKey!=button) :
+                    print(print_button)
+                    oldKey=button
+            GPIO.output(SCLPin,GPIO.HIGH)
+            time.sleep(HALF_BIT_TIME)
+
+            button+=1
+
+        pressed=False
+
+except KeyboardInterrupt:
+    pass
+    GPIO.cleanup()
+
+
---
+++ Raspberry_Pi_SBC_code/sensors/TTP229_touch_keypad/TTP229_test02.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# TTP229_test02.py shows how the TTP229 touch keypad is used 
+#  uses two of the spare GPIO poins onthe PCB
+# Author : Enmore Green Limited
+# Date   : 210511
+# command to run this script:  python3 ./RPi_maker_kit5/sensors/TTP229_touch_keypad/TTP229_test02.py
+#  command above to be updated for the user's path to the code
+
+import RPi.GPIO as GPIO
+import time
+
+
+"""No. of keys"""
+inputKeys=16
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) 
+""" SCL and SDO pin can be any pin """
+SCLPin=18  #using a spare GPIO pin on the RPi Maker Kit PCB
+SDOPin=15  #using a spare GPIO pin on the RPi Maker Kit PCB
+
+"""
+Set SCL pin as OUTPUT
+Set SDO pin as INPUT
+"""
+
+GPIO.setup(SCLPin,GPIO.OUT)
+GPIO.setup(SDOPin,GPIO.IN)
+
+keyPressed=0
+
+def getKey():
+        button=0
+        global keyPressed
+        keyState=0
+        time.sleep(0.05)
+
+        """
+        Sample the Clock pin 16 times and read the data pin,
+        when touched data pin is recorded LOW.
+        """
+        for i in range(inputKeys):
+                GPIO.output(SCLPin,GPIO.LOW)
+                if not GPIO.input(SDOPin):
+                        keyState=i+1
+                GPIO.output(SCLPin, GPIO.HIGH)
+                
+        if (keyState>0 and keyState!=keyPressed):
+                button=keyState
+                keyPressed=keyState
+        else:
+                keyPressed=keyState
+        return (button)
+        
+        
+
+while True:
+        key=getKey()
+        if(key>0):
+                print("key " + str(key) + " pressed")
+        
